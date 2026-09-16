8d1e98f2cc9a6d2a60412d68776716d7db178ad4
Jessica Chon <jc3932@cornell.edu>
2026-09-16 18:31:22 -0400

    modified original screen_clock.py to have timed reactions

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -2,21 +2,21 @@ import time
 import subprocess
 import digitalio
 import board
+from time import strftime
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 
-# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
-cs_pin = digitalio.DigitalInOut(board.D5) 
+# Configuration for CS and DC pins
+cs_pin = digitalio.DigitalInOut(board.D5)
 dc_pin = digitalio.DigitalInOut(board.D25)
 reset_pin = None
 
-# Config for display baudrate (default max is 24mhz):
 BAUDRATE = 64000000
 
-# Setup SPI bus using hardware SPI:
+# Setup SPI bus
 spi = board.SPI()
 
-# Create the ST7789 display:
+# Create the ST7789 display
 disp = st7789.ST7789(
     spi,
     cs=cs_pin,
@@ -29,43 +29,115 @@ disp = st7789.ST7789(
     y_offset=40,
 )
 
-# Create blank image for drawing.
-# Make sure to create image with mode 'RGB' for full color.
-height = disp.width  # we swap height/width to rotate it to landscape!
+# Display dimensions
+height = disp.width
 width = disp.height
-image = Image.new("RGB", (width, height))
 rotation = 90
 
-# Get drawing object to draw on image.
+# Create image
+image = Image.new("RGB", (width, height))
 draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+# Clear screen
+draw.rectangle((0, 0, width, height), fill=0)
 disp.image(image, rotation)
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height - padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+# Font
+font = ImageFont.truetype(
+    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
+    18
+)
 
-# Turn on the backlight
+# Turn on backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+
+# --------------------------------
+# Load and prepare red.jpg
+# --------------------------------
+
+image = Image.open("red.jpg").convert("RGB")
+
+# Resize image to fit the display
+image_ratio = image.width / image.height
+screen_ratio = width / height
+
+if screen_ratio < image_ratio:
+    scaled_width = image.width * height // image.height
+    scaled_height = height
+else:
+    scaled_width = width
+    scaled_height = image.height * width // image.width
+
+image = image.resize(
+    (scaled_width, scaled_height),
+    Image.BICUBIC
+)
+
+# Crop and center
+x = scaled_width // 2 - width // 2
+y = scaled_height // 2 - height // 2
+
+image = image.crop(
+    (x, y, x + width, y + height)
+)
+
+# --------------------------------
+# Main loop
+# --------------------------------
+
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    current_time = strftime("%H:%M:%S")
 
-    # Display image.
-    disp.image(image, rotation)
+    # --------------------------------
+    # 6:26:00 PM - 6:26:30 PM
+    # Show red.jpg
+    # --------------------------------
+
+    if "18:26:00" <= current_time <= "18:26:30":
+
+        disp.image(image, rotation)
+
+
+    # --------------------------------
+    # 6:26:31 PM - 6:26:59 PM
+    # Show red.jpg + text
+    # --------------------------------
+
+    elif "18:26:31:" <= current_time <= "18:26:59":
+
+        # Make a copy so we don't permanently
+        # draw the text onto the original image
+        display_image = image.copy()
+
+        display_draw = ImageDraw.Draw(display_image)
+
+        display_draw.text(
+            (10, 10),
+            "YOUR TEXT HERE",
+            font=font,
+            fill="black"
+        )
+
+        disp.image(display_image, rotation)
+
+
+    # --------------------------------
+    # Outside those times
+    # --------------------------------
+
+    else:
+
+        # Black screen
+        black = Image.new(
+            "RGB",
+            (width, height),
+            (0, 0, 0)
+        )
+
+        disp.image(black, rotation)
+
     time.sleep(1)
